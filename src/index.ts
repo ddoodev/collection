d48b014bd2df66ee738abdb5e5f663537f828202
Kakarot <kakarot.joel@gmail.com>
2021-08-18 19:44:19 +0530

    chore: reduce extra code used by exports (#4)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
-import { CollectionEqualOptions } from '@src/interfaces/CollectionEqualOptions'
-import { CollectionFilterOptions } from '@src/interfaces/CollectionFilterOptions'
-import { CollectionRandomOptions } from '@src/interfaces/CollectionRandomOptions'
-import { Collection } from './Collection'
-
-export {
-  CollectionEqualOptions,
-  CollectionFilterOptions,
-  CollectionRandomOptions,
-  Collection
-}
+export * from '@src/interfaces'
+export { Collection } from './Collection'
