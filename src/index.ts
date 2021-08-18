98207462837a020d72c950f61d05c29d52b5005f
Kakarot <kakarot.joel@gmail.com>
2021-08-18 19:34:31 +0530

    checking smth

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
-import { CollectionEqualOptions } from '@src/interfaces/CollectionEqualOptions'
-import { CollectionFilterOptions } from '@src/interfaces/CollectionFilterOptions'
-import { CollectionRandomOptions } from '@src/interfaces/CollectionRandomOptions'
-import { Collection } from './Collection'
-
-export {
-  CollectionEqualOptions,
-  CollectionFilterOptions,
-  CollectionRandomOptions,
-  Collection
-}
+export { CollectionEqualOptions } from '@src/interfaces/CollectionEqualOptions'
+export { Collection } from './Collection'
